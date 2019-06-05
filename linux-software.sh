546a9086758fba260a6db980e7e51d5fe2e9e582
Dan Ferguson <dan.ferguson@chpw.org>
2019-06-05 15:31:15 -0700

    added linux-software.sh

---
+++ linux-software.sh
@@ -0,0 +1,4 @@
+!/bin/bash
+
+echo "Installing baseline software for Ubuntu in a Windows domain"
+sudo apt install openssh-server net-tools git tmux vim xclip nmap bash-completion git-completion file make lynis autofs lastpass-cli krb5-user samba sssd chrony -y
