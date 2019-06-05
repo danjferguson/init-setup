e31189d121bbfc67d9c25a3fee287386284cd316
Dan Ferguson <dan.ferguson@chpw.org>
2019-06-05 14:59:22 -0700

    removed curl

--- init-linux.sh
+++ init-linux.sh
@@ -1,12 +1,12 @@
 !/bin/bash
 
 echo "Installing baseline software packages"
-sudo apt install openssh-server net-tools git tmux vim xclip nmap bash-completion git-completion curl file make lynis autofs -y
+sudo apt install openssh-server net-tools git tmux vim xclip nmap bash-completion git-completion file make lynis autofs -y
 
 echo "Installing LinuxBrew"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 
-echo "Creating directory strucgture"
+echo "Creating directory structure"
 mkdir -p $HOME/src/github/danjferguson/{priv,pub}
 mkdir -p $HOME/var/{bak,bin,etc,opt,scratch}
 mkdir -p $HOME/.vim/colors
