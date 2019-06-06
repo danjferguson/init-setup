43630bdd879acff8b63b62a850c57f8231d596bf
Dan Ferguson <dan.ferguson@chpw.org>
2019-06-06 13:40:27 -0700

    change software install

--- ubuntu-ad-server.sh
+++ ubuntu-ad-server.sh
@@ -1,4 +1,8 @@
 !/bin/bash
 
-echo "Installing baseline software for Ubuntu in a Windows domain"
-sudo apt install openssh-server net-tools git tmux vim xclip nmap bash-completion git-completion file make lynis autofs lastpass-cli krb5-user samba sssd chrony -y
+echo "Installing baseline software for Ubuntu member server in Windows domain"
+sudo apt install software-properties-common net-tools git tmux vim xclip nmap bash-completion git-completion file make lynis autofs krb5-user samba sssd chrony -y
+
+echo "Adding Ansible repository and installing"
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt install ansible
