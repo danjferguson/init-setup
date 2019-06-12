fd11448bd1fd77e175178bee758730039da7b81f
Dan Ferguson <dan.ferguson@chpw.org>
2019-06-12 12:18:17 -0700

    updated software

--- init-linux.sh
+++ init-linux.sh
@@ -1,12 +1,8 @@
-!/bin/bash
+#!/bin/bash
 
 echo "Installing baseline software packages"
-sudo apt install openssh-server net-tools git tmux vim xclip nmap bash-completion git-completion file make lynis autofs -y
+sudo apt install software-properties-common openssh-server net-tools git tmux vim xclip nmap bash-completion git-completion file make lynis autofs lastpass-cli krb5-user samba sssd chrony ruby -y
 
-echo "Installing LinuxBrew"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-
-echo "Creating directory structure"
-mkdir -p $HOME/src/github/danjferguson/{priv,pub}
-mkdir -p $HOME/var/{bak,bin,etc,opt,scratch}
-mkdir -p $HOME/.vim/colors
+echo "Adding Ansible repository and installing"
+sudo apt-add-repository ppa:ansible/ansible -s -y
+sudo apt install ansible -y
