f781b81d047ceb371e6e25b2266426ff57d28679
Dan Ferguson <dan.ferguson@chpw.org>
2019-06-12 12:01:58 -0700

    moved tmuxinator install

--- init-ad-user.sh
+++ init-ad-user.sh
@@ -4,3 +4,9 @@ echo "Creating directory structure"
 mkdir -p $HOME/src/github/danjferguson/{priv,pub}
 mkdir -p $HOME/var/{bak,bin,etc,opt,scratch}
 mkdir -p $HOME/.vim/colors
+echo "Installing tmuxinator"
+sudo gem install tmuxinator
+mkdir $HOME/.bin
+curl https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.bash -so $HOME/.bin/tmuxinator.bash
+echo 'export EDITOR='vim'' >> $HOME/.bashrc
+echo 'export source $HOME/.bin/tmuxinator.bash' >> $HOME/.bashrc
--- linux-software.sh
+++ linux-software.sh
@@ -6,10 +6,3 @@ sudo apt install software-properties-common openssh-server net-tools git tmux vi
 echo "Adding Ansible repository and installing"
 sudo apt-add-repository ppa:ansible/ansible -s -y
 sudo apt install ansible -y
-
-echo "Installing tmuxinator"
-sudo gem install tmuxinator
-mkdir $HOME/.bin
-curl https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.bash -so $HOME/.bin/tmuxinator.bash
-echo 'export EDITOR='vim'' >> $HOME/.bashrc
-echo 'export source $HOME/.bin/tmuxinator.bash' >> $HOME/.bashrc
