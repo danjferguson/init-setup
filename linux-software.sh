7413f9fb486df01a16dd5b8f76e5bf556276b053
Dan Ferguson <dan.ferguson@chpw.org>
2019-06-13 08:45:27 -0700

    removed linux-software.sh

--- linux-software.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Installing baseline software for Ubuntu desktop in a Windows domain"
-sudo apt install software-properties-common openssh-server net-tools git tmux vim xclip nmap bash-completion git-completion file make lynis autofs lastpass-cli krb5-user samba sssd chrony ruby -y
-
-echo "Adding Ansible repository and installing"
-sudo apt-add-repository ppa:ansible/ansible -s -y
-sudo apt install ansible -y
