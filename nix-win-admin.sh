24ad3b45a75961c7314ef11d4713d3877bd09d66
Dan Ferguson <daniel@securenetwork.tech>
2019-06-14 10:23:04 -0700

    added nix-win-admin.sh

---
+++ nix-win-admin.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Installing Windows admin tools"
+
+# Download the Microsoft repository GPG keys
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+
+# Register the Microsoft repository GPG keys
+sudo dpkg -i packages-microsoft-prod.deb
+
+# Update the list of products
+sudo apt update
+
+# Enable the "universe" repositories
+sudo add-apt-repository universe
+
+# Install PowerShell
+sudo apt install -y powershell
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code chromium-browser gimp -y
