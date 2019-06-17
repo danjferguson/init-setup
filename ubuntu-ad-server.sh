15caa6385d1f4f9e7eec5a8f2bc974d15bc18ba9
Dan <daniel@securenetwork.tech>
2019-06-17 15:00:19 -0700

    deleted

--- ubuntu-ad-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Installing baseline software for Ubuntu member server in Windows domain"
-sudo apt install software-properties-common net-tools git tmux vim xclip nmap bash-completion git-completion file make lynis autofs krb5-user samba sssd chrony -y
-
-echo "Adding Ansible repository and installing"
-sudo apt-add-repository ppa:ansible/ansible -s -y
-sudo apt install ansible -y
