fe0c8e691827395fe61618064d632b4e9ed4e809
Dan Ferguson <daniel@securenetwork.tech>
2019-06-19 09:45:18 -0700

    added weechat

--- init-linux.sh
+++ init-linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing baseline software packages"
-sudo apt install software-properties-common openssh-server net-tools git tmux vim xclip nmap bash-completion git-completion file make lynis autofs lastpass-cli krb5-user samba sssd chrony ruby python-pip -y
+sudo apt install software-properties-common python-minimal openssh-server net-tools git tmux vim xclip nmap bash-completion git-completion file make lynis autofs lastpass-cli krb5-user samba sssd chrony ruby python-pip -y
 
 echo "Adding Ansible repository and installing"
 sudo apt-add-repository ppa:ansible/ansible -s -y
