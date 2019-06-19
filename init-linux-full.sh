93dab1f30ba5ea3f9f8ef6c9cd709f9572cade05
Dan Ferguson <daniel@securenetwork.tech>
2019-06-19 15:46:03 -0700

    added more software

--- init-linux-full.sh
+++ init-linux-full.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
 echo "Installing baseline software packages"
-sudo apt install software-properties-common python-minimal openssh-server net-tools git tmux vim xclip nmap bash-completion git-completion file make lynis autofs lastpass-cli krb5-user samba sssd chrony ruby python-pip -y
+sudo apt install software-properties-common python-minimal openssh-server net-tools git tmux vim xclip nmap bash-completion git-completion file make lynis autofs lastpass-cli krb5-user samba sssd chrony ruby python-pip weechat chromium-browser gimp -y
 
 echo "Adding Ansible repository and installing"
 sudo apt-add-repository ppa:ansible/ansible -s -y
 sudo apt install ansible -y
 
-echo "Installing Tmuxinator"
+echo "Installing Tmuxinator via Ruby"
 sudo gem install tmuxinator
+
+echo "Installing TeamViewer via secure repository"
+wget -O - https://download.teamviewer.com/download/linux/signature/TeamViewer2017.asc | apt-key add -
+sudo sh -c 'echo "deb http://linux.teambiwere.com/deb stable main" >> /etc/apt/sources.list.d/teamviewer.list'
+sudo apt update
+sudo apt install teamviewer -y
+
+echo "Installing Remmina repository and installing"
+sudo apt-add-repository ppa:remmina-ppa-team/remmina-next
+sudo apt install remmina remmina-plugin-rdp remmina-plugin-secret -y
+
+echo "Don't forget to install AV from Sophos -- it is free!"
+echo "https://www.sophos.com/en-us/products/free-tools/sophos-antivirus-for-linux.aspx"
