2a217865c8b36585d1ae34d32e2507c322a96b7a
Dan Ferguson <daniel@securenetwork.tech>
2019-06-24 10:37:20 -0700

    streamlined

--- init-linux-full.sh
+++ init-linux-full.sh
@@ -2,28 +2,19 @@
 
 echo "Adding third party repos and keys"
 cd /tmp
-wget https://download.teamviewer.com/download/linux/signature/TeamViewer2017.asc
-apt-key add TeamViewer2017.asc
-sudo sh -c 'echo "deb http://linux.teamviewer.com/deb stable main" >> /etc/apt/sources.list.d/teamviewer.list'
+# Adding ansible repository
 sudo apt-add-repository ppa:ansible/ansible -s -y
-sudo apt-add-repository ppa:remmina-ppa-team/remmina-next
-wget -O vmware.bin https://www.vmware.com/go/getWorkstation-linux
+# Refresh list of products
 sudo apt update
 
 echo "Installing baseline software packages"
-sudo apt install software-properties-common python-minimal openssh-server net-tools git tmux vim xclip nmap bash-completion git-completion file make lynis autofs lastpass-cli ruby python-pip weechat chromium-browser gimp gparted ansible teamviewer remmina remmina-plugin-rdp remmina-plugin-secret -y
+sudo apt install software-properties-common python-minimal openssh-server net-tools git tmux vim xclip nmap bash-completion git-completion file make lynis autofs lastpass-cli ruby python-pip weechat chromium-browser gimp gparted ansible -y
 
 echo "Installing Tmuxinator via Ruby"
 sudo gem install tmuxinator
 
-echo "Installing newest version of VMWare Workstation"
-sudo bash vmware.bin
-
 echo "Don't forget to install AV from Sophos -- it is free!"
 echo "https://www.sophos.com/en-us/products/free-tools/sophos-antivirus-for-linux.aspx"
-echo "Joining a Windows domain? run init-samba.sh"
-curl -O https://raw.githubusercontent.com/danjferguson/init-setup/master/init-samba.sh
-chmod +x init-samba.sh
 echo "To complete a Windows administrator box on ubuntu download nix-win-admin.sh"
 curl -O https://raw.githubusercontent.com/danjferguson/init-setup/master/nix-win-admin.sh
 chmod +x nix-win-admin.sh
--- nix-win-admin.sh
+++ nix-win-admin.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
-echo "Installing Windows admin tools"
-if ! [ -f "$HOME/.config/powershell" ]; then
-  mkdir $HOME/.config/powershell
-fi
+cd /tmp
+# Downloading secure key and adding repo for TeamViewer
+wget https://download.teamviewer.com/download/linux/signature/TeamViewer2017.asc
+apt-key add TeamViewer2017.asc
+sudo sh -c 'echo "deb http://linux.teamviewer.com/deb stable main" >> /etc/apt/sources.list.d/teamviewer.list'
+# Adding repository for remmina, RDP client
+sudo apt-add-repository ppa:remmina-ppa-team/remmina-next
+# Downloading latest verison of VMWare Workstation
+wget -O vmware.bin https://www.vmware.com/go/getWorkstation-linux
 # Download the Microsoft repository GPG keys
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
-
 # Register the Microsoft repository GPG keys
 sudo dpkg -i packages-microsoft-prod.deb
-
-# Update the list of products
-sudo apt update
-
-# Enable the "universe" repositories
-sudo add-apt-repository universe
-
-# Install PowerShell
-sudo apt install -y powershell
-
+# VS Code Installer
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-
+# Update the list of products
+sudo apt update
+# Enable the "universe" repositories
+sudo add-apt-repository universe
+echo "Installing Windows administrator software tools"
+if ! [ -f "$HOME/.config/powershell" ]; then
+  mkdir $HOME/.config/powershell
+fi
 sudo apt install apt-transport-https
 sudo apt update
-sudo apt install code -y
+sudo apt install teamviewer powershell code -y
+echo "Installing newest version of VMWare Workstation"
+sudo bash vmware.bin
+
+echo "Joining a Windows domain? run init-samba.sh"
+curl -O https://raw.githubusercontent.com/danjferguson/init-setup/master/init-samba.sh
+chmod +x init-samba.sh
