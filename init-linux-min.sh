ec1990227cdedd5e7df149576108a329ed8c4ff5
Dan Ferguson <daniel@securenetwork.tech>
2019-06-24 09:49:20 -0700

    streamlined

---
+++ init-linux-min.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Installing baseline software packages"
+sudo apt install openssh-server tmux vim bash-completion file make python-minimal python-pip ruby -y
--- init-linux-samba.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Installing baseline software packages"
-sudo apt install software-properties-common python-minimal openssh-server tmux vim bash-completion file make krb5-user samba sssd chrony python-pip -y
